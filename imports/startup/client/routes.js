36b210f6812c1ae0b6f7248cd4c90a4a2fcd0fc5
stephdion <stephane.dion@gmail.com>
2017-02-19 22:14:52 -0500

    routes

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -4,6 +4,7 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 // Import needed templates
 import '../../ui/layouts/body/body.js';
 import '../../ui/pages/home/home.js';
+import '../../ui/pages/googlemap/googlemap.js';
 import '../../ui/pages/not-found/not-found.js';
 
 // Set up all routes in the app
@@ -11,6 +12,13 @@ FlowRouter.route('/', {
   name: 'App.home',
   action() {
     BlazeLayout.render('App_body', { main: 'App_home' });
+  },
+});
+
+FlowRouter.route('/map', {
+
+  action() {
+    BlazeLayout.render('App_body', { main: 'googlemap' });
   },
 });
 
---
+++ imports/ui/pages/googlemap/googlemap.js
@@ -0,0 +1,12 @@
+
+
+import { Meteor } from 'meteor/meteor';
+import './googlemap.html';
+
+
+Template.googlemap.rendered = function () {
+
+    
+
+
+};
